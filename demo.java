774d7a13442c64006555ff27178c4d270b42fb5c
unknown <jvjagadishkumar@yahoo.com>
2018-11-28 15:24:24 +0530

    added tea

--- demo.java
+++ demo.java
@@ -1,17 +1,10 @@
-<<<<<<< HEAD
 class Softdrinks{
 public static void main(String[] args)
 {
 System.out.println("pepsi");
 System.out.println("cococola");
 System.out.println("milk");
-=======
-class Softdrinks
-{
-public static void main(String[] args)
-{
-System.out.println("cococola");
-System.out.println("pepsi");
->>>>>>> 944e36122ab45cce5da0ff961c0fc75b75888b33
+System.out.println("tea");
 }
 }
+
